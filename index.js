51a152f6d7f6d77d3369e341ba6707ffa29ba973
Yael Harosh <yaelharosh01@gmail.com>
2023-02-21 14:22:55 -0500

    pass all scope tests

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'initial value';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'bob';
+}
